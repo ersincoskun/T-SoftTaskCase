d254f6bb7cb6d852fc2ca94bcb24e831a6b01cc4
ecoskun <ecoskun@invio.com>
2023-10-13 18:55:32 +0300

    main activity fragmentları ve main nav graph oluşturuldu.

---
+++ app/src/main/java/com/tsoft/taskcase/ui/fragment/FavoritesFragment.kt
@@ -0,0 +1,8 @@
+package com.tsoft.taskcase.ui.fragment
+
+import com.tsoft.taskcase.base.BaseFragment
+import com.tsoft.taskcase.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>() {
+
+}
---
+++ app/src/main/java/com/tsoft/taskcase/ui/fragment/ImageListFragment.kt
@@ -0,0 +1,8 @@
+package com.tsoft.taskcase.ui.fragment
+
+import com.tsoft.taskcase.base.BaseFragment
+import com.tsoft.taskcase.databinding.FragmentImageListBinding
+
+class ImageListFragment : BaseFragment<FragmentImageListBinding>() {
+
+}
