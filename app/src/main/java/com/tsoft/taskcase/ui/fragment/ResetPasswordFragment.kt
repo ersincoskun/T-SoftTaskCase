8084f9dc8f2fa143c6ea681616cdf2e5a04db6e7
ecoskun <ecoskun@invio.com>
2023-10-13 17:46:12 +0300

    reset password sayfası tasarımı tamamlandı.

---
+++ app/src/main/java/com/tsoft/taskcase/ui/fragment/ResetPasswordFragment.kt
@@ -0,0 +1,8 @@
+package com.tsoft.taskcase.ui.fragment
+
+import com.tsoft.taskcase.base.BaseFragment
+import com.tsoft.taskcase.databinding.FragmentResetPasswordBinding
+
+class ResetPasswordFragment : BaseFragment<FragmentResetPasswordBinding>() {
+
+}
