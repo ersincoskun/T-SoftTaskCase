a929b44a87647f2894e4a9e90a073dc7247b637a
ecoskun <ecoskun@invio.com>
2023-10-15 17:00:55 +0300

    api level 34 de çıkan pending intent crash'i fixlendi.

--- app/src/main/java/com/tsoft/taskcase/utils/Constants.kt
+++ app/src/main/java/com/tsoft/taskcase/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.tsoft.taskcase.utils
 
 object Constants {
     const val PREF_NAME="TSOFT_TASK_PREF_NAME"
-    const val MEET_NOW_LOG_TAG = "TSOFT_TASK_LOG_TAG"
+    const val TSOFT_TASK_LOG_TAG = "TSOFT_TASK_LOG_TAG"
 }
--- app/src/main/java/com/tsoft/taskcase/utils/Utils.kt
+++ app/src/main/java/com/tsoft/taskcase/utils/Utils.kt
@@ -1,12 +1,12 @@
 package com.tsoft.taskcase.utils
 
 import android.util.Log
-import com.tsoft.taskcase.utils.Constants.MEET_NOW_LOG_TAG
+import com.tsoft.taskcase.utils.Constants.TSOFT_TASK_LOG_TAG
 
 fun printLog(logText: String) {
-    Log.d(MEET_NOW_LOG_TAG, logText)
+    Log.d(TSOFT_TASK_LOG_TAG, logText)
 }
 
 fun printErrorLog(logText: String) {
-    Log.e(MEET_NOW_LOG_TAG, logText)
+    Log.e(TSOFT_TASK_LOG_TAG, logText)
 }
